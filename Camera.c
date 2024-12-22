d8c9740009197bb7c71fe5e58f081c7df8379ffe
Moataz <moatazaitmeziane20@gmail.com>
2024-12-22 16:26:22 +0000

    added basics for camera management

---
+++ Camera.c
@@ -0,0 +1,71 @@
+#include "Camera.h"
+
+void CameraMove(Camera2D * Camera) {
+
+    Vector2 delta = GetMouseDelta();
+    delta = Vector2Scale(delta, -1.0f/Camera->zoom);
+    Camera->target = Vector2Add(Camera->target, delta);
+}
+
+void CameraMoveBorder(Camera2D * Camera,int height,int width) {
+    Vector2 offseter =GetMouseDelta();
+    printf("the x is : %f and they y is : %f \n",offseter.x,offseter.y);
+    Vector2 mouse = GetMousePosition();
+    Vector2 delta = {0,0};
+
+    if(mouse.x>=width-10){
+
+        if(offseter.y>0){
+            delta.y+=3;
+        }
+        else if(offseter.y<0){
+            delta.y-=3;
+        }
+
+        delta.x+=3;
+    }
+    if(mouse.x<=10){
+             if(offseter.y>0){
+            delta.y+=3;
+        }
+        else if(offseter.y<0){
+            delta.y-=3;
+        }
+        delta.x-=3;
+    }
+    if(mouse.y>=height-10){
+        if(offseter.x>0){
+            delta.x+=3;
+        }
+        else if(offseter.x<0){
+            delta.x-=3;
+        }
+        delta.y+=3;
+    }
+    if(mouse.y<=10 ){
+            if(offseter.x>0){
+            delta.x+=3;
+        }
+        else if(offseter.x<0){
+            delta.x-=3;
+        }
+        delta.y-=3;
+    }
+
+
+    delta = Vector2Scale(delta,1.0f * 5.0f/Camera->zoom );
+    Camera->target=Vector2Add(Camera->target,delta);
+
+}
+
+/*bool MouseOnBorder(int width,int height) {
+    Vector2 mouse = GetMousePosition();
+    //printf("mouse y : %f \n",mouse.y);
+
+    if(mouse.x>=width-3 || mouse.x<=3 || mouse.y<=3 || mouse.y>=height-3) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}*/
---
+++ Camera.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Utils.h"
+
+void CameraMove(Camera2D * Camera);
+void CameraMoveBorder(Camera2D * Camera,int height,int width) ;
+bool MouseOnBorder(int width,int height);
